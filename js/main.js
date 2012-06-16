2a9cfb0519b68481cf047a66a403a3964710f54c
Jay Stewart <jay@busyanimal.com>
2012-06-16 21:44:30 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-
   /**
   * Spinner
   */
@@ -18,7 +17,6 @@ jQuery(document).ready(function() {
     });
     return this;
   };
-
   /**
   * Infinte scrolling
   */
@@ -60,9 +58,8 @@ jQuery(document).ready(function() {
     pixelsFromNavToBottom: undefined,
     path: undefined
   });
-
   /**
-  * Initialises the jPlayer audio players on the page
+  * Initialises the audio players
   */
   function init_audios() {
     $.each($(".cp-jplayer"), function(index, value) {
@@ -77,98 +74,38 @@ jQuery(document).ready(function() {
       );
     });
   }
-
   /**
-  * Initialize talk object
+  * Set variables
   */
-  var talk = {
-      name: '',
-      option: 'talks',
-      list: [],
-      page: 1    
-  };
-
+  var searched;
+  var page = 1;
+  /**
+  * Bind search form
+  */
   $('.form-search').submit(function(e) {
+    // Start the spinner
     $('.spin').spin();
     e.preventDefault();
-    talk.name = $('.input-search').val();
+    searched = $('.input-search').val();
+    // Clear the search form for next search
     $('.input-search').val(null);
-    $.getJSON('http://dharma-api.com/talks?api_key=c5257b4edb77e1d35a797d5ffc9b4691&rpp=10&search=' + talk.name + '&page=' + talk.page + '&callback=?',
-      function(response) {
-        // Cache the results
-        talk.list = response.results;
-        talk.metta = response.metta;
-        if(!!talk.metta) {
-          var metta =   '<ul>';
-              metta +=    '<li><span>Total:</span>"' + talk.metta.total + '"</li>';
-              metta +=    '<li><span>Ordered by:</span>"' + talk.metta.ordered_by + '"</li>';
-              metta +=    '<li><span>Loving Kindness:</span>"' + talk.metta.loving_kindness + '"</li>';
-              metta +=  '</ul>';
-              $('.metta').html(metta);
+    /**
+    * Make call to dharma-api.com and render the results
+    */
+    $.getJSON('http://dharma-api.com/talks?api_key=c5257b4edb77e1d35a797d5ffc9b4691&rpp=10&search=' + searched + '&page=' + page,
+        function lovely(response) {
+          var data = response;
+          if(!!data) {
+            html = new EJS({url: 'talks.ejs'}).render(data);
+            $('.results').html(html);
+            // Initialize the new players
+            init_audios();
+            // Stop the spinner
+            $('.spin').html(null);
+          }
         }
-        if(!!talk.list) {
-          $('.results').html(null);
-          $(talk.list).each(function(i, item) {
-            var picture;
-            if(typeof item.speaker.picture == 'undefined') {
-              picture = 'img/nopic.jpg';
-            } else {
-              picture = item.speaker.picture;
-            }
-            var description;
-            if(typeof item.description == 'undefined') {
-              description = '';
-            } else {
-              description = item.description;
-            }
-
-            var results =   '<div class="row_item row-fluid">';
-                results +=    '<div class="span2">';
-                results +=      '<div class="img_div">';
-                results +=        '<img src="' + picture + '" ' + 'class="speakers_img">';
-                results +=      '</div>';
-                results +=    '</div>';
-                results +=    '<div class="span7">';
-                results +=      '<ul class="talk_details">';
-                results +=        '<li><h1 class="title">' + item.title + '</h1></li>';
-                results +=        '<li><span>Description:</span>' + description + '</li>';
-                results +=        '<li><span>Speaker:</span>' + item.speaker.name + '</li>';
-                results +=        '<li><span>Date:</span>' + item.date + '</li>';
-                results +=        '<li><span>Duration:</span>' + item.duration + '</li>';
-                results +=        '<li><span>Source:</span>' + item.source + '</li>';
-                results +=        '<li><span>Licence:</span><a href="' + item.license + '">Creative Commons</a></li>';
-                results +=        '<li><a href="' + item.permalink + '"><span>Download &rarr;</span></a></li>';
-                results +=      '</ul>';
-                results +=    '</div>';
-                results +=    '<div class="span2">';
-                results +=      '<div id="jquery_jplayer_' + item.id + '" class="cp-jplayer" rel="' + item.permalink + '"></div>';
-                results +=      '<div id="cp_container_' + item.id + '" class="cp-container">';
-                results +=        '<div class="cp-buffer-holder"> <!-- .cp-gt50 only needed when buffer is > than 50% -->';
-                results +=          '<div class="cp-buffer-1"></div>';
-                results +=          '<div class="cp-buffer-2"></div>';
-                results +=        '</div>';
-                results +=        '<div class="cp-progress-holder"> <!-- .cp-gt50 only needed when progress is > than 50% -->';
-                results +=          '<div class="cp-progress-1"></div>';
-                results +=          '<div class="cp-progress-2"></div>';
-                results +=        '</div>';
-                results +=        '<div class="cp-circle-control"></div>';
-                results +=        '<ul class="cp-controls">';
-                results +=          '<li><a href="#" class="cp-play" tabindex="1">play</a></li>';
-                results +=          '<li><a href="#" class="cp-pause" style="display:none;" tabindex="1">pause</a></li> <!-- Needs the inline style here, or jQuery.show()uses display:inline instead of display:block -->';
-                results +=        '</ul>';
-                results +=      '</div>';
-                results +=    '</div>';
-                results +=  '</div>';                       
-                $(results).appendTo('.results')   
-
-          });
-          init_audios();
-          $('.spin').html(null);
-        }
-      }
     );
   });
-  init_audios();
+  // Focus cursor in search box on page load
   $('.input-search').focus();
-
 });
