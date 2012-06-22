85e8490acb5c28a590b856a85fa55cd717592666
Jay Stewart <jay@busyanimal.com>
2012-06-22 20:18:55 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var _gaq = [];
+_gaq.push(['_setAccount', 'UA-31671486-5'], ['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $('.about').click(function () {
   $('#about').modal();
 });
